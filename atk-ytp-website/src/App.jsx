1cc796d3bfe438b34d55b35155796ffa496c7a3a
paakkis <juusop2k12@gmail.com>
2023-01-16 19:37:04 +0200

    initial commit

---
+++ atk-ytp-website/src/App.jsx
@@ -0,0 +1,15 @@
+import { ReactComponent as Triangle } from "./assets/triangle.svg";
+import './App.scss'
+import Header from "./components/Header";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ atk-ytp-website/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.scss'
+import { ReactComponent as Triangle } from "../assets/triangle.svg";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1 className='a'>ATK-YTP</h1>
+            <h1 className='b'>JOENSUU</h1>
+        </div>
+ 
+    )
+}
+
+export default Header
