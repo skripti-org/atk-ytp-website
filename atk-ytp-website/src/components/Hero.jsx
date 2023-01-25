0abb1d72a21b29219547122ee5f7561b1d332f24
paakkis <juusop2k12@gmail.com>
2023-01-25 15:12:13 +0200

    scrollaavat tekstit

--- atk-ytp-website/src/components/Hero.jsx
+++ atk-ytp-website/src/components/Hero.jsx
@@ -1,13 +1,21 @@
 import './Hero.scss'
+import TextScroller from './TextScroller'
 
 const Hero = () => {
     return (
         
         <div className='hero-container'>
+            <div className='text-scroller-container'>
+                <TextScroller/>
+            </div>
+            
             <iframe className="hero-image" src="https://cables.gl/view/63418132a1293e86e06c980a"></iframe>
                 <div className="hero-content">
                     <h1 className='a'>ATK-YTP</h1>
                     <h1 className='b'>JOENSUU</h1>
+            </div>
+            <div className='bottom-text-scroller-container'>
+                <TextScroller/>
             </div>
         </div>
  
---
+++ atk-ytp-website/src/components/TextScroller.jsx
@@ -0,0 +1,16 @@
+import './TextScroller.scss'
+
+const TextScroller = () => {
+    return (
+        <div class="wrapper">
+            <p class="marquee">
+                <span>ATK YTP JOENSUU 18.10.2023 - ATK YTP JOENSUU 18.10.2023 - ATK YTP JOENSUU 18.10.2023 - &nbsp;</span>
+            </p>
+            <p class="marquee marquee2">
+                <span>ATK YTP JOENSUU 18.10.2023 - ATK YTP JOENSUU 18.10.2023 - ATK YTP JOENSUU 18.10.2023 -&nbsp;</span>
+            </p>
+        </div>
+    )
+}
+
+export default TextScroller
