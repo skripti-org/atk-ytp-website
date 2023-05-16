dfd7c236cb9c99728dcfbf941e51add9cee212bf
Juho Nykänen <juho.nykanen@hotmail.fi>
2023-05-16 09:58:01 +0300

    leivottu vähän tuota shaderia 225°C 20min

--- atk-ytp-website/src/App.jsx
+++ atk-ytp-website/src/App.jsx
@@ -1,18 +1,17 @@
-import Navbar from "./components/Navbar"
-import About from "./components/about/About"
-import Hero from "./components/hero/Hero"
-
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/hero/Hero';
 
 function App() {
-
-
   return (
-    <div className="App">
+    <div className='App'>
+      {' <YtpHelmet /> '}
       <Navbar />
       <Hero />
-      
+
+      <canvas id='webgl-canvas'></canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
