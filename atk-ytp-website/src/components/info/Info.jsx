0ec0a379f2cbe8596bfa44f0d03cb4f65e999851
Eetu Karttunen <eetu.karttunen@outlook.com>
2023-08-08 15:03:33 +0300

    Sommitelmaa taustavärjätylle Title-boksille

--- atk-ytp-website/src/components/info/Info.jsx
+++ atk-ytp-website/src/components/info/Info.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import PageSection from '../layout/PageSection';
-import { Title } from '@mantine/core';
+import { Button, Container, Title } from '@mantine/core';
 
 export default function Info() {
   return (
-    <PageSection title='Info'>
-      <Title order={3}>Majoitus</Title>
-      <Title order={3}>Liikkuminen</Title>
-      <Title order={3}>Kartta</Title>
-    </PageSection>
+    <>
+      <PageSection title='Info'></PageSection>
+      {/* Raaka sommitelma taustavärjätylle titlelle */}
+      <Container sx={{ margin: 0, "max-width": "100%" }} bg='triangle-yellow'>
+        <Title color="dark" order={2} pt={20} pb={20}>Tietoa</Title>
+      </Container>
+      <PageSection>
+        <Title order={3}>Majoitus</Title>
+        <Title order={3}>Liikkuminen</Title>
+        <Title order={3}>Kartta</Title>
+      </PageSection>
+    </>
   );
 }
