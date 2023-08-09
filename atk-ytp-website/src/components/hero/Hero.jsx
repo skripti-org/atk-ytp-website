51fb478b196553adf809c1a240757ae549357a12
juuso.paakkunainen <juusop2k12@gmail.com>
2023-08-09 11:34:33 +0300

    pientä viilausta

--- atk-ytp-website/src/components/hero/Hero.jsx
+++ atk-ytp-website/src/components/hero/Hero.jsx
@@ -5,12 +5,12 @@ import Counter from './Counter';
 const Hero = () => {
   return (
     <section className='h-[100vh] py-2'>
-      <div className='container mx-auto flex flex-col justify-center py-12 lg:flex-row lg:justify-evenly xl:py-36'>
-        <div className='flex flex-col justify-center rounded-sm p-6 text-left lg:max-w-md xl:max-w-lg'>
-          <h1 className='m-0 font-sans text-5xl font-extrabold mix-blend-difference sm:text-9xl'>
+      <div className='container mx-auto flex flex-col justify-center py-20 lg:flex-row lg:justify-evenly xl:py-36'>
+        <div className='flex flex-col justify-center rounded-sm p-6 text-center lg:max-w-md xl:max-w-lg'>
+          <h1 className='m-0 font-sans text-7xl font-extrabold mix-blend-difference sm:text-9xl'>
             ATK YTP
           </h1>
-          <h1 className='font-sans text-xs mix-blend-difference sm:text-3xl'>
+          <h1 className='font-sans text-sm mix-blend-difference sm:text-3xl'>
             18-19.10.2023
             <br />
             JOENSUU
@@ -31,7 +31,7 @@ const Hero = () => {
           </Container>
         </div>
       </div>
-      <div className='xl:h-112 2xl:h-128 flex h-72 items-center justify-center sm:h-80 lg:mt-0 lg:h-3'>
+      <div className='xl:h-112 2xl:h-128 flex h-72 items-center justify-center px-4 sm:h-80 lg:mt-0 lg:h-3'>
         <Counter targetDate={new Date('2023-10-18T10:00:00Z').getTime()} />
       </div>
     </section>
