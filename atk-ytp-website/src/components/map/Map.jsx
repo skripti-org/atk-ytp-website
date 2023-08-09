3a6cb40154874616ae91f2e538b5f82421736dae
paakkis <juusop2k12@gmail.com>
2023-08-09 18:02:40 +0300

    legend mutta paskana

--- atk-ytp-website/src/components/map/Map.jsx
+++ atk-ytp-website/src/components/map/Map.jsx
@@ -3,19 +3,60 @@ import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import PageSection from '../layout/PageSection';
 import React from 'react';
+import './Legend.css'
 
 export default function Map() {
+
   const playerIcon = new Icon({
-    iconUrl: 'https://sampwiki.blast.hk/wroot/images2/a/ad/Icon_2.gif',
-    iconSize: [40, 40],
+    iconUrl: 'https://sampwiki.blast.hk/wroot/images2/3/3e/Icon_3.gif',
+    iconSize: [30, 30],
   });
 
   function CustomIcon({ iconUrl }) {
     return new Icon({
       iconUrl: iconUrl,
-      iconSize: [40, 40],
+      iconSize: [30, 30],
     });
   }
+
+  function Legend() {
+    const map = useMap();
+  
+    const legend = L.control({ position: "bottomright" });
+    const div = L.DomUtil.create("div", "info legend");
+
+    legend.onAdd = () => {
+      const types = [
+        {
+          type: 'Majoitus',
+          iconUrl: 'https://sampwiki.blast.hk/wroot/images2/4/41/Icon_35.gif',
+        },
+        {
+            type: 'Baari',
+            iconUrl: 'https://sampwiki.blast.hk/wroot/images2/a/a4/Icon_49.gif',
+        },
+        {
+          type: 'Ravintola',
+          iconUrl: 'https://sampwiki.blast.hk/wroot/images2/3/3a/Icon_50.gif',
+        },
+      ];
+      const labels = [];
+      types.map(type => {
+        labels.push(
+          `<img src="${type.iconUrl}" alt="${type.type}" width="20" className="legend-img"/> 
+          <span className="legend-text">${type.type}</span>`
+          
+        );
+      });
+      div.innerHTML = labels.join("<br>");
+      return div;
+    };
+  
+    legend.addTo(map);
+  
+    return null; // No need to render anything for the Legend component
+  }
+  
 
   function LocationMarker() {
     const [position, setPosition] = React.useState(null);
@@ -41,8 +82,23 @@ export default function Map() {
     const markers = [
       {
         coordinates: [62.6020939, 29.7623925],
-        label: 'Joku baari vissii :D',
+        label: 'Marks',
         iconUrl: 'https://sampwiki.blast.hk/wroot/images2/a/a4/Icon_49.gif',
+      },
+      {
+        coordinates: [62.6026422, 29.7745023],
+        label: 'Pielisjoen koulu',
+        iconUrl: 'https://sampwiki.blast.hk/wroot/images2/4/41/Icon_35.gif',
+      },
+      {
+        coordinates: [62.6036733,29.7446238,21],
+        label: 'Carelia sali',
+        iconUrl: 'https://sampwiki.blast.hk/wroot/images2/7/7b/Icon_36.gif',
+      },
+      {
+        coordinates: [62.60358808111174, 29.747125070027504],
+        label: 'Aurora sali',
+        iconUrl: 'https://sampwiki.blast.hk/wroot/images2/7/7b/Icon_36.gif',
       },
     ];
     return (
@@ -83,8 +139,10 @@ export default function Map() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
             />
+           
             <LocationMarker />
             <Markers />
+            <Legend />
           </MapContainer>
         </div>
       </section>
