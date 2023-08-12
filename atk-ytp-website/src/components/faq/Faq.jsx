b21e81f2ae0df87627c81ac43b3d943f469d307c
Juho Nykänen <juho.nykanen@hotmail.fi>
2023-08-12 15:24:09 +0300

    hifi

--- atk-ytp-website/src/components/faq/Faq.jsx
+++ atk-ytp-website/src/components/faq/Faq.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Accordion, Blockquote } from '@mantine/core';
 import PageSection from '../layout/PageSection';
 import HighlightSection from '../layout/HighlightSection';
 import { Carousel } from '@mantine/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+
 const Q = 0;
 const A = 1;
 const qas = [
@@ -26,35 +28,44 @@ const quotes = [
     text: 'Wine if fine, but whiskey is quicker.',
     person: 'Hank Moody',
   },
-
 ];
 const FAQ = () => {
+  const autoplay = useRef(Autoplay({ delay: 2000 }));
+
   return (
     <PageSection title='FAQ'>
       <Accordion variant='separated' radius='0'>
         {qas.map((qa) => {
           return (
-          
-              <Accordion.Item key={qa[Q]} value={qa[Q]}>
-                <Accordion.Control>{qa[Q]}</Accordion.Control>
-                <Accordion.Panel>{qa[A]}</Accordion.Panel>
-              </Accordion.Item>
-         
+            <Accordion.Item key={qa[Q]} value={qa[Q]}>
+              <Accordion.Control>{qa[Q]}</Accordion.Control>
+              <Accordion.Panel>{qa[A]}</Accordion.Panel>
+            </Accordion.Item>
           );
         })}
       </Accordion>
 
       <HighlightSection>
-        <Carousel mx="auto" withIndicators height={200}>
+        <Carousel
+          mx='auto'
+          withIndicators
+          loop
+          height={200}
+          plugins={[autoplay.current]}
+          onMouseEnter={autoplay.current.stop}
+          onMouseLeave={autoplay.current.reset}
+          orientation='vertical'
+        >
           {quotes.map((quote) => {
-              return (
-                <Carousel.Slide className='flex items-center text-center justify-center'>
-                  <Blockquote cite={'- ' + quote.person}>
-                    {quote.text}
-                  </Blockquote>
-                </Carousel.Slide>
-              );
-            })}
+            return (
+              <Carousel.Slide
+                key={quote.text}
+                className='flex items-center justify-center text-center'
+              >
+                <Blockquote cite={'- ' + quote.person}>{quote.text}</Blockquote>
+              </Carousel.Slide>
+            );
+          })}
         </Carousel>
       </HighlightSection>
     </PageSection>
--- atk-ytp-website/src/components/info/Info.jsx
+++ atk-ytp-website/src/components/info/Info.jsx
@@ -1,39 +1,31 @@
 import React from 'react';
 import PageSection from '../layout/PageSection';
-import HighlightSection from '../layout/HighlightSection';
-import { Accordion, Blockquote } from '@mantine/core';
-
-
 import { Anchor, Alert } from '@mantine/core';
 import Paragraph from '../layout/Paragraph';
 import { IconAlertCircle } from '@tabler/icons-react';
-import { Carousel } from '@mantine/carousel';
-const Q = 0;
-const A = 1;
+
 export default function Info() {
-
-
   return (
-    <div id="info">
+    <div id='info'>
       <PageSection title='Info'>
-
-
-
         <Paragraph title='Majoitus'>
-          Nukutaan jos nukutaan.
-          <br />
-          termos!
+          Tapahtuman majoitus tapahtuu kouluilla x ja y. Majoitukset avautuvat tapahtumapäivinä klo
+          x ja sulkeutuvat klo x. Muistathan pitää majoituspaikan siistinä ja siivota omat jälkesi!
         </Paragraph>
 
         <Paragraph title='Ruoka'>
-          Näläkäinen nikotteleksen, kyllin syönyt röyhteleksen
+          Näläkäinen nikotteleksen, kyllin syönyt röyhteleksen.
+          <br />
+          <br />
+          Joensuun kampukselta löytyy useita opiskelijaravintoloita tapahtumapaikan läheltä, kuten
+          Carelia, Kampus Bistro ja Wicked Rabbit (kasvisruoka). Opiskelijaravintoloiden lisäksi
+          Joensuun keskusta on pullollaan erilaisia ruokapaikkoja. Näiden lisäksi Foodora ja Wolt
+          kuljettavat sapuskaa.
+          <br />
           <br />
           Kurkkaappa joutessas:{' '}
-          <Anchor
-            href=' https://www.uef.fi/fi/kampusravintolat'
-            target='_blank'
-          >
-          Joensuun kampusravintolat ja -kahvilat.
+          <Anchor href=' https://www.uef.fi/fi/kampusravintolat' target='_blank'>
+            Joensuun kampusravintolat ja -kahvilat.
           </Anchor>
         </Paragraph>
 
@@ -51,53 +43,42 @@ export default function Info() {
         </Paragraph>
 
         <Paragraph title='Turvallinen tila'>
-        Tapahtumassa noudatetaan turvallisen tilan periaatteita.
-        Yhdenvertaisuuslain mukaan ketään ei saa syrjiä iän, alkuperän, kansalaisuuden, kielen, uskonnon, vakaumuksen, mielipiteen, poliittisen toiminnan, ammattiyhdistystoiminnan, perhesuhteiden, terveydentilan, vammaisuuden, seksuaalisen suuntautumisen, tai muun henkilöön liittyvän syyn perusteella. 
-        <br/><br/>
-        Turvallisemmilla tiloilla viitataan yhteisölliseen pyrkimykseen luoda turvallisuutta lisääviä käytäntöjä. Käytännössä turvallisemmissa tiloissa ainejärjestö ja sen jäsenet pyrkivät ottamaan vastuuta hankalista tilanteista. Ristiriitoja käsitellään sen sijaan, että niitä vain siedetään.
-        <br/><br/>
-        Lisätietoja tapahtumassa voimassa olevista turvallisen tilan periaatteissa löydät 
-        
-        <Anchor
-            href='https://www.skripti.org/turvallinentila'
-            target='_blank'
-          > Skripti ry:n weppisivuilta.</Anchor>
-
+          Tapahtumassa noudatetaan turvallisen tilan periaatteita. Yhdenvertaisuuslain mukaan ketään
+          ei saa syrjiä iän, alkuperän, kansalaisuuden, kielen, uskonnon, vakaumuksen, mielipiteen,
+          poliittisen toiminnan, ammattiyhdistystoiminnan, perhesuhteiden, terveydentilan,
+          vammaisuuden, seksuaalisen suuntautumisen, tai muun henkilöön liittyvän syyn perusteella.
+          <br />
+          <br />
+          Turvallisemmilla tiloilla viitataan yhteisölliseen pyrkimykseen luoda turvallisuutta
+          lisääviä käytäntöjä. Käytännössä turvallisemmissa tiloissa ainejärjestö ja sen jäsenet
+          pyrkivät ottamaan vastuuta hankalista tilanteista. Ristiriitoja käsitellään sen sijaan,
+          että niitä vain siedetään.
+          <br />
+          <br />
+          Lisätietoja tapahtumassa voimassa olevista turvallisen tilan periaatteissa löydät
+          <Anchor href='https://www.skripti.org/turvallinentila' target='_blank'>
+            {' '}
+            Skripti ry:n weppisivuilta.
+          </Anchor>
         </Paragraph>
 
         <Paragraph title='Linkkejä'>
-          <Anchor
-            href='https://jojo.joensuu.fi/liput-ja-hinnat'
-            target='_blank'
-          >
+          <Anchor href='https://jojo.joensuu.fi/liput-ja-hinnat' target='_blank'>
             Joukkoliikenne JoJo
           </Anchor>
           <br />
-          <Anchor
-            href=''
-            target='_blank'
-          >
+          <Anchor href='' target='_blank'>
             ATK-YTP Telegram
           </Anchor>
           <br />
-          <Anchor
-            href=''
-            target='_blank'
-          >
-          Skripti ry
+          <Anchor href='https://www.skripti.org' target='_blank'>
+            Skripti ry
           </Anchor>
           <br />
-          <Anchor
-            href='https://www.skripti.org/turvallinentila'
-            target='_blank'
-          >
+          <Anchor href='https://www.skripti.org/turvallinentila' target='_blank'>
             Skripti ry:n turvallisen tilan periaatteet
           </Anchor>
         </Paragraph>
-
-
-
-
       </PageSection>
     </div>
   );
