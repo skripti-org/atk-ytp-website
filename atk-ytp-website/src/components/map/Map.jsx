db49caef25761a2c8cd20036dff9c8168ba0d24c
paakkis <juusop2k12@gmail.com>
2023-08-15 00:14:25 +0300

    mapin refaktorointi

--- atk-ytp-website/src/components/map/Map.jsx
+++ atk-ytp-website/src/components/map/Map.jsx
@@ -6,25 +6,13 @@ import {
   useMap,
   Popup,
   useMapEvent,
-} from 'react-leaflet';
-import { Icon } from 'leaflet';
+} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PageSection from '../layout/PageSection';
 import { useRef } from 'react';
-import { IconInfoCircleFilled } from '@tabler/icons-react';
-
-const playerIcon = new Icon({
-  iconUrl: 'https://sampwiki.blast.hk/wroot/images2/3/3e/Icon_3.gif',
-  iconSize: [30, 30],
-});
-
-function CustomIcon({ iconUrl }) {
-  return new Icon({
-    iconUrl: iconUrl,
-    iconSize: [30, 30],
-  });
-}
+import Legend from './Legend';
+import CustomIcon from './CustomIcon';
 
 function Markers() {
   const map = useMap();
@@ -41,12 +29,12 @@ function Markers() {
     },
     {
       coordinates: [62.6036733, 29.7446238, 21],
-      label: 'Carelia sali',
+      label: 'Carelia-sali',
       iconUrl: 'https://sampwiki.blast.hk/wroot/images2/7/7b/Icon_36.gif',
     },
     {
       coordinates: [62.60358808111174, 29.747125070027504],
-      label: 'Aurora sali',
+      label: 'Aurora-sali',
       iconUrl: 'https://sampwiki.blast.hk/wroot/images2/7/7b/Icon_36.gif',
     },
   ];
@@ -77,7 +65,6 @@ function LocationMarker() {
   const [position, setPosition] = React.useState(null);
 
   const map = useMap();
-
   React.useEffect(() => {
     map.locate().on('locationfound', function (e) {
       setPosition(e.latlng);
@@ -85,112 +72,11 @@ function LocationMarker() {
   }, [map]);
 
   return position === null ? null : (
-    <Marker position={position} icon={playerIcon}>
+    <Marker position={position} icon={CustomIcon({iconUrl:'https://sampwiki.blast.hk/wroot/images2/3/3e/Icon_3.gif'})}>
       <Popup>Olet tässä :D</Popup>
     </Marker>
   );
 }
-const Legend = () => {
-  const [showLegend, setShowLegend] = useState(false);
-
-  const toggleLegend = () => {
-    setShowLegend(!showLegend); // Step 2
-  };
-
-  const legendStyle = {
-    backgroundColor: '#1A1B1E',
-    textAlign: 'left',
-    borderRadius: '5px',
-    boxShadow: '0 1px 2px rgba(0, 0, 0, 0.1)',
-    minWidth: '15%',
-    minHeight: '25%',
-    margin: '1rem',
-    color: 'white',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    cursor: 'pointer',
-  };
-
-  const infoContainer = {
-    display: showLegend ? 'none' : 'flex',
-    backgroundColor: '#1A1B1E',
-    textAlign: 'left',
-    borderRadius: '5px',
-    boxShadow: '0 1px 2px rgba(0, 0, 0, 0.1)',
-    zIndex: 1000,
-    width: '2.5rem',
-    height: '2.5rem',
-    margin: '1rem',
-    color: 'white',
-    display: 'flex',
-    cursor: 'pointer',
-
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-
-  const types = [
-    {
-      type: 'Majoitus',
-      iconUrl: 'https://sampwiki.blast.hk/wroot/images2/4/41/Icon_35.gif',
-    },
-    {
-      type: 'Baari',
-      iconUrl: 'https://sampwiki.blast.hk/wroot/images2/a/a4/Icon_49.gif',
-    },
-    {
-      type: 'Ravintola',
-      iconUrl: 'https://sampwiki.blast.hk/wroot/images2/3/3a/Icon_50.gif',
-    },
-    {
-      type: 'Luentosali',
-      iconUrl: 'https://sampwiki.blast.hk/wroot/images2/7/7b/Icon_36.gif',
-    },
-  ];
-
-  return (
-    <div className='leaflet-top leaflet-right'>
-      {showLegend ? (
-        <div
-          className='legend leaflet-control-zoom leaflet-control-container leaflet-control px-4'
-          style={legendStyle}
-          onClick={toggleLegend}
-        >
-          <div className='py-2 text-center font-sans'>
-            <strong className=''>Info</strong>
-          </div>
-          <div className='flex flex-1 flex-col justify-center py-2'>
-            {types.map((type, index) => {
-              return (
-                <div className='flex items-center'>
-                  <div>
-                    <img
-                      src={type.iconUrl}
-                      alt={type.type}
-                      width={20}
-                      style={{ marginRight: '5px' }}
-                    />
-                  </div>
-                  <div className='font-sans'>{type.type}</div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      ) : (
-        <div
-          className='leaflet-control-zoom leaflet-control-container leaflet-control'
-          style={infoContainer}
-          onClick={toggleLegend}
-        >
-          <IconInfoCircleFilled />
-        </div>
-      )}
-    </div>
-  );
-};
 
 function SetViewOnClick({ animateRef }) {
   const map = useMapEvent('click', (e) => {
@@ -225,6 +111,7 @@ export default function Map() {
               <LocationMarker />
               <Markers />
               <Legend />
+   
             </MapContainer>
           </div>
         </section>
