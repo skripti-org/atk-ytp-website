39f286dc60ef3c64af8ce4f139f0dd5e00b75d32
nyksy <71010049+nyksy@users.noreply.github.com>
2023-09-26 17:41:58 +0300

    herossa vähän layouttia tiiviimmäksi

--- atk-ytp-website/src/components/hero/Hero.jsx
+++ atk-ytp-website/src/components/hero/Hero.jsx
@@ -15,64 +15,68 @@ const Hero = () => {
   const { classes } = useStyles();
   return (
     <section className='relative grid min-h-[calc(100dvh-59px)]'>
-      <div className='container mx-auto flex flex-col justify-center p-8'>
-        <Container className='relative flex flex-col text-center lg:max-w-md xl:max-w-lg'>
-          <picture>
-            <source srcSet={logoWebp} type='image/webp' className='max-w-full' />
-            <img className='max-w-full' src={logo} alt='ATK-YTP logo' />
-          </picture>
+      <div>
+        <div className='container mx-auto flex flex-col justify-center px-8 py-2'>
+          <Container className='relative flex flex-col text-center lg:max-w-md xl:max-w-lg'>
+            <picture>
+              <source srcSet={logoWebp} type='image/webp' className='max-w-full' />
+              <img className='max-w-full' src={logo} alt='ATK-YTP logo' />
+            </picture>
 
-          <h1 className='font-outline-2-subtitle font-sans text-2xl sm:text-3xl'>18.-20.10.2023</h1>
+            <h1 className='font-outline-2-subtitle font-sans text-2xl sm:text-3xl'>
+              18.-20.10.2023
+            </h1>
+          </Container>
+        </div>
+
+        <Container className='px-8 text-center'>
+          <Button
+            sx={classes.heroBtn}
+            color='triangle-yellow'
+            radius='xl'
+            component='a'
+            href='#aikataulu'
+            rightIcon={<IconCalendar />}
+            styles={() => ({
+              root: {
+                color: 'black',
+              },
+            })}
+          >
+            Aikataulu
+          </Button>
+          <Button
+            sx={classes.heroBtn}
+            color='pink'
+            radius='xl'
+            component='a'
+            href='#traileri'
+            rightIcon={<IconMovie />}
+            styles={() => ({
+              root: {
+                color: 'white',
+              },
+            })}
+          >
+            Traileri
+          </Button>
+          <Button
+            sx={classes.heroBtn}
+            color='triangle-yellow'
+            radius='xl'
+            component='a'
+            href='#kartta'
+            rightIcon={<IconMap />}
+            styles={() => ({
+              root: {
+                color: 'black',
+              },
+            })}
+          >
+            Kartta
+          </Button>
         </Container>
       </div>
-
-      <Container className='p-8 text-center'>
-        <Button
-          sx={classes.heroBtn}
-          color='triangle-yellow'
-          radius='xl'
-          component='a'
-          href='#aikataulu'
-          rightIcon={<IconCalendar />}
-          styles={() => ({
-            root: {
-              color: 'black',
-            },
-          })}
-        >
-          Aikataulu
-        </Button>
-        <Button
-          sx={classes.heroBtn}
-          color='pink'
-          radius='xl'
-          component='a'
-          href='#traileri'
-          rightIcon={<IconMovie />}
-          styles={() => ({
-            root: {
-              color: 'white',
-            },
-          })}
-        >
-          Traileri
-        </Button>
-        <Button
-          sx={classes.heroBtn}
-          color='triangle-yellow'
-          radius='xl'
-          component='a'
-          href='#kartta'
-          rightIcon={<IconMap />}
-          styles={() => ({
-            root: {
-              color: 'black',
-            },
-          })}
-        >
-          Kartta
-        </Button>
-      </Container>
       <Container className='flex items-center justify-center p-8'>
         <Counter targetDate={new Date('2023-10-18T10:00:00Z').getTime()} />
       </Container>
